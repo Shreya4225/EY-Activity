124642a0f755e6a2664f4cda7a199d37cf6621f3
Shreya4225 <121634043+Shreya4225@users.noreply.github.com>
2025-09-29 12:15:56 +0530

    Add files via upload

--- Sep29-Day6/Tasks/school_join_queries.sql
+++ Sep29-Day6/Tasks/school_join_queries.sql
@@ -1,1 +1,48 @@
+CREATE database school;
+use school
 
+create table teachers(
+teacher_id int auto_increment primary key,
+name varchar(50),
+subject_id int
+);
+
+create table subjects(
+subject_id int auto_increment primary key,
+subject_name varchar(50)
+);
+
+INSERT INTO subjects (subject_name) VALUES
+('Mathematics'),   -- id = 1
+('Science'),       -- id = 2
+('English'),       -- id = 3
+('History'),       -- id = 4
+('Geography');     -- id = 5 (no teacher yet)
+
+INSERT INTO teachers (name, subject_id) VALUES
+('Rahul Sir', 1),   -- Mathematics
+('Priya Madam', 2), -- Science
+('Arjun Sir', NULL),-- No subject assigned
+('Neha Madam', 3);  -- English
+
+select * from teachers
+
+select s.subject_name, t.name, t.teacher_id
+from subjects s
+LEFT JOIN teachers t on s.subject_id = t.subject_id
+
+select s.subject_name, t.name, t.teacher_id
+from subjects s
+RIGHT JOIN teachers t on s.subject_id = t.subject_id
+
+select s.subject_name, t.name, t.teacher_id
+from subjects s
+LEFT JOIN teachers t on s.subject_id = t.subject_id
+UNION
+select s.subject_name, t.name, t.teacher_id
+from subjects s
+RIGHT JOIN teachers t on s.subject_id = t.subject_id
+
+select s.subject_name, t.name, t.teacher_id
+from subjects s
+INNER JOIN teachers t on s.subject_id = t.subject_id
