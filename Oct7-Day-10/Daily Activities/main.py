52ab25c3dbf9ba3b02aece9c3f23b1233d970140
Shreya4225 <121634043+Shreya4225@users.noreply.github.com>
2025-10-07 21:00:15 +0530

    Add files via upload

--- Oct7-Day-10/Daily Activities/main.py
+++ Oct7-Day-10/Daily Activities/main.py
@@ -1,1 +1,46 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
+app = FastAPI()
+class Student(BaseModel):
+    id: int
+    name: str
+    age: int
+    course: str
+
+students = [
+    {"id": 1, "name": "Rahul", "age": 21, "course": "AI"},
+    {"id": 2, "name": "Priya", "age": 22, "course": "ML"},
+]
+
+@app.get("/students")
+def get_students():
+    return {"students": students}
+
+@app.get("/students/{student_id}")
+def get_student(student_id: int):
+    for student in students:
+        if student["id"] == student_id:
+            return student
+    raise HTTPException(status_code=404, detail="student not found")
+
+@app.post(path="/students", status_code=201)
+def add_student(student: Student):
+    students.append(student.dict())
+    return{"message": "student added successfully", "student": student}
+
+@app.put("/students/{student_id}")
+def update_student(student_id: int, updated_student: Student):
+    for i, s in enumerate(students):
+        if s["id"] == student_id:
+            students[i] = updated_student.dict()
+            return {"message": "Student updated", "student": updated_student}
+    raise HTTPException(status_code=404, detail="student not found")
+
+@app.delete("/students/{student_id}")
+def delete_student(student_id: int):
+    for i, s in enumerate(students):
+        if s["id"] == student_id:
+            students.pop(i)
+            return {"message": "Student deleted", "student": s}
+    raise HTTPException(status_code=404, detail="student not found")
