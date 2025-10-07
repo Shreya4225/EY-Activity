109af97bbe2a5d6da48689901cf7cbe6bba21a23
Shreya4225 <121634043+Shreya4225@users.noreply.github.com>
2025-10-07 21:02:05 +0530

    Add files via upload

--- Oct7-Day-10/Tasks/employee_api.py
+++ Oct7-Day-10/Tasks/employee_api.py
@@ -1,1 +1,50 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+class Employee(BaseModel):
+    id: int
+    name : str
+    department : str
+    salary : float
+
+employees = [
+    {"id": 1, "name": "Atharva", "department": "AI", "salary": "AI"},
+    {"id": 2, "name": "Prajakta", "department": "ML", "salary": "ML"},
+]
+
+@app.get("/employees")
+def get_employees():
+    return {"employees" : employees}
+
+@app.get("/employees/{employee_id}")
+def get_employee(employee_id: int):
+    for employee in employees:
+        if employee["id"] == employee_id:
+            return employee
+    raise HTTPException(status_code=404, detail="student not found")
+
+@app.post(path= "/employees", status_code=201)
+def add_employee(employee: Employee):
+    employees.append(employee.dict())
+    return{"message": "employee added successfully", "employee": employee}
+
+@app.put("/employees/{employee_id}")
+def update_employee(employee_id: int, updated_employee: Employee):
+    for i, s in enumerate(employees):
+        if s["id"] == employee_id:
+            employees[i] = updated_employee.dict()
+            return{"message": "employee updated successfully", "employee": employees[i]}
+    raise HTTPException(status_code=404, detail="employee not found")
+
+@app.delete("/employees/{employee_id}")
+def delete_employee(employee_id: int):
+    for i, s in enumerate(employees):
+        if s["id"] == employee_id:
+            employees.pop(i)
+            return{"message": "employee deleted successfully", "employee": s}
+    raise HTTPException(status_code=404, detail="employee not found")
+
+
+
 
